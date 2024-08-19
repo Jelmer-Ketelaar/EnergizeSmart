9744b90d5c9baeba8915c2028630fdd1d20860d3
Jelmer Ketelaar <jelmerketelaar487@gmail.com>
2024-08-19 08:56:16 +0200

    Added pages to navbar and to routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EnergyPredictionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EnergyDataController;
 
@@ -7,9 +8,12 @@ Route::get('/', function () {
     return view('dashboard');
 });
 
-Route::get('daily/{userId}', [EnergyDataController::class, 'showDaily'])->name('energy.daily');
-Route::get('/compare-weeks/{userId}', [EnergyDataController::class, 'compareWeeks'])->name('energy.weekly');
+Route::get('/daily/{userId}', [EnergyDataController::class, 'showDaily'])->name('energy.daily');
+Route::get('/weekly/{userId}', [EnergyDataController::class, 'showWeekly'])->name('energy.weekly');
+Route::get('/monthly/{userId}', [EnergyDataController::class, 'showMonthly'])->name('energy.monthly');
+Route::get('/compare-weeks/{userId}', [EnergyDataController::class, 'compareWeeks'])->name('energy.compare');
 
+// These routes should be protected to require authentication
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -19,3 +23,10 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::resource('energy_predictions', EnergyPredictionController::class);
+
+Route::patch('energy_predictions/{energyPrediction}/update-actual', [EnergyPredictionController::class, 'updatePredictionWithActualValue'])
+    ->name('energy_predictions.update_actual');
+
+
